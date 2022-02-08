6261bdc5e13987f8da92ea30ab5323a63a6b67bc
Mizanali Panjwani <mizanalip786@gmail.com>
2022-02-08 13:39:40 +0530

    Added font and configured theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+
+      primary: '#FF7979',
+      secondary: '#FFA95A',
+
+      white: '#ffffff',
+      black: '#000000',
+
+      background: '#1b1b1b',
+    },
+    fontFamily: {
+      body: ['Montserrat'],
+    },
   },
   plugins: [],
 };
