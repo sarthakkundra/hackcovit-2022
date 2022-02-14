95560ded42016aac464c50e5c4b28204d60b3839
Mizanali Panjwani <mizanalip786@gmail.com>
2022-02-15 00:16:22 +0530

    Added Themes section

---
+++ app/components/Themes/Themes.tsx
@@ -0,0 +1,16 @@
+import ThemesCarousel from '../ThemesCarousel/ThemesCarousel';
+
+const Themes = () => {
+  return (
+    <div>
+      <h1 className="text-xl md:text-4xl font-semibold text-secondary text-center my-5">
+        Themes
+      </h1>
+      <div className="mx-auto w-3/4 my-5">
+        <ThemesCarousel />
+      </div>
+    </div>
+  );
+};
+
+export default Themes;
